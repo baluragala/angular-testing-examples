5cf6029b371bfdeec34f739d055538f7f1261caa
Balakrishna Ragala <baluragala@gmail.com>
2017-11-10 07:49:04 +0530

    git remote add origin https://github.com/baluragala/angular-testing-examples.git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule }         from '@angular/core';
+import { BrowserModule }    from '@angular/platform-browser';
+
+import { AppComponent }     from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AboutComponent }   from './about.component';
+import { BannerComponent }  from './banner.component';
+import { UserService }      from './model/user.service';
+import { HeroService } from './model/hero.service';
+import { TwainService }     from './shared/twain.service';
+import { WelcomeComponent } from './welcome.component';
 
 
-import { AppComponent } from './app.component';
-
+import { DashboardModule }  from './dashboard/dashboard.module';
+import { SharedModule }     from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DashboardModule,
+    AppRoutingModule,
+    SharedModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers:    [ HeroService, TwainService, UserService ],
+  declarations: [ AppComponent, AboutComponent, BannerComponent, WelcomeComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
